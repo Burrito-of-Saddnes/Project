960baddb3ff407c3701e336d02b13e4bc554c58c
Burrito-of-Saddnes <61121665+Burrito-of-Saddnes@users.noreply.github.com>
2020-10-01 18:28:10 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Component } from 'react';
 import Index from './src/Index';
 
+
+//Расскидать все по папкам и под комплюхтеры когда-нибудь стили сделать
 export default class App extends Component {
   render() {
     return (
